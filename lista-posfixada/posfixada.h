6cb02744e224fbbc32855734bc4d04f3b519b473
diogommartins <diogo.martins@unirio.br>
2015-03-18 19:28:13 -0300

    Adicionado método privado is_digito para encapsular lógica de verificação de operandos

--- lista-posfixada/posfixada.h
+++ lista-posfixada/posfixada.h
@@ -10,10 +10,11 @@ private:
     char *expressao;	// Expressao posfixada
     int tamanho;      // Tamanho da expressao posfixada
     
-    // verifica se sibolo eh um operador?
+    // verifica se simbolo eh um operador?
     bool operador(const char simbolo) const;
     // Aplica operador a 2 operandos e informa erros
     bool aplica_operador(const char Operator);
+    bool is_digito(const char c);
     
 public:
     posfixada(int tamanho_pilha = 10, int tamanho_expressao = 50);
@@ -21,6 +22,6 @@ public:
     void le_expressao();
     // Avalia expressao posfixada e reporta erros
     bool avalia_expressao();
-    void imprime_resultado() const;
+    void imprime_resultado();
 };
 #endif
