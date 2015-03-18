82b36d33486aaeda7613d49d3454a49e8d198949
diogommartins <diogo.martins@unirio.br>
2015-03-18 18:24:45 -0300

    Initial Commit

---
+++ lista-posfixada/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lista-posfixada
+//
+//  Created by Diogo Martins on 3/18/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
