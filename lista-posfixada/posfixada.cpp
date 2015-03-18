9a80fadc1750e68cabdbd3f83fd2bb4b689f695e
diogommartins <diogo.martins@unirio.br>
2015-03-18 19:46:26 -0300

    Adicionadas implementações de char_to_int, is_digito e avalia_expressão

--- lista-posfixada/posfixada.cpp
+++ lista-posfixada/posfixada.cpp
@@ -33,3 +33,49 @@ bool posfixada::operador(const char simbolo) const{
     }
     return false;
 }
+
+bool posfixada::aplica_operador(const char Operator){
+    return false;
+}
+
+bool posfixada::is_digito(const char c){
+    int n = int(c);
+    if (n >= int('0') && n <= int('9')){
+        return true;
+    }
+    return false;
+}
+
+int posfixada::char_to_int(const char c){
+    return int(c) - int('0');
+}
+
+bool posfixada::avalia_expressao(){
+    int i = 0;
+    char c;
+    
+    while (expressao[i] != '\n'){
+        c = expressao[i];
+        if (operador(c)){
+            aplica_operador(c);
+        }
+        else if (posfixada::is_digito(c)){
+            pilha.push(char_to_int(c));
+        }
+        i++;
+    }
+    if (!pilha.pop(resposta)){
+        cout << "ERRO ao desempilhar \n";
+        return false;
+    }
+    else if(! pilha.vazia()){
+        /* 
+         Ao desempilhar, o unico elemento deve ser o resultado. Se sobrar alguma coisa
+         algo deu errado
+         */
+        cout << "ERRO \n";
+        return false;
+    }
+    return true;
+}
+
